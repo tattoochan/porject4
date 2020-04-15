6b7d49b6127178b71cf1613cd3e00b7bcdf2c7d9
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2020-04-15 16:34:23 +0000

    Route to login function and render login form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from accounts.views import index, logout
+from accounts.views import index, logout, login
 
 urlpatterns = [
     path('', index, name='index'),
     path('logout/', logout, name='logout'),
+    path('login/', login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import auth, messages
-
+from accounts.forms import LoginForm
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -9,3 +9,9 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully been logged out")
     return redirect(reverse('index'))
+    
+def login(request):
+    login_form = LoginForm()
+    return render(request, 'login.html', {
+        'form':login_form
+    })
