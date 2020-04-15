28a28b6c6690ddd187dfd051f54006cd6cae0f4b
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2020-04-15 16:42:54 +0000

    setup done for login page

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,25 @@ def logout(request):
     return redirect(reverse('index'))
     
 def login(request):
-    login_form = LoginForm()
-    return render(request, 'login.html', {
-        'form':login_form
-    })
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST) # populate the form from what the user has keyed in
+        if login_form.is_valid():
+            # attempt to check the username and password is valid
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            messages.success(request, "You have successfully logged in")
+            if user:
+                # log in the user
+                auth.login(user=user, request=request)
+                return redirect(reverse('index'))
+            else:
+                login_form.add_error(None, "Invalid username or password")
+                return render(request, 'login.html', {
+                  'form':login_form
+                })
+
+    else:
+        login_form = LoginForm()
+        return render(request, 'login.html', {
+            'form':login_form
+        })
